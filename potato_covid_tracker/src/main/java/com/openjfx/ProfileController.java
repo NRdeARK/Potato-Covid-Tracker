ee12298404f5fbec6e87a875fe1caa80d6b1780e
Nataphan rakvidhyasatra <85008989+NRdeARK@users.noreply.github.com>
2022-05-23 00:18:34 +0700

    update

--- potato_covid_tracker/src/main/java/com/openjfx/ProfileController.java
+++ potato_covid_tracker/src/main/java/com/openjfx/ProfileController.java
@@ -8,7 +8,6 @@ import javafx.scene.image.ImageView;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.sql.Date;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ResourceBundle;
