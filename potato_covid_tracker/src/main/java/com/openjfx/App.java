6cb8222c518cbc14dd039d688bfadd0f19c6fe2c
Nataphan rakvidhyasatra <85008989+NRdeARK@users.noreply.github.com>
2022-05-27 00:34:39 +0700

    update app ,about Us

--- potato_covid_tracker/src/main/java/com/openjfx/App.java
+++ potato_covid_tracker/src/main/java/com/openjfx/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,7 +14,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("images/login/logo.png")));
         stage.setResizable(false);
+        stage.setTitle("POTATO COVID TRACKER");
         Parent root = FXMLLoader.load(getClass().getResource("fxml/launch.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
