84bfbd5c2d6b3e707aebe51b556546d4a5815d45
Nataphan rakvidhyasatra <85008989+NRdeARK@users.noreply.github.com>
2022-05-27 14:39:36 +0700

    update

--- potato_covid_tracker/src/main/java/com/openjfx/CityController.java
+++ potato_covid_tracker/src/main/java/com/openjfx/CityController.java
@@ -173,7 +173,7 @@ public class CityController implements Initializable {
 
     @FXML
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        displayUsername();
+        // displayUsername();
         displayCityData(1);
         CityComboBox.getItems().addAll(cityList);
         CityComboBox.setOnAction(event -> {
@@ -409,10 +409,10 @@ public class CityController implements Initializable {
     @FXML
     public void updateButton(ActionEvent event) throws IOException {
         System.out.println("start update city data");
+        UpdateButton.setText();
         List<String> lines = new ArrayList<String>();
         APIConnector apiConnecter = new APIConnector("https://covid19.ddc.moph.go.th/api/Cases/");
         JSONArray jsonArray = apiConnecter.getJSONArray("today-cases-by-provinces");
-
         for (int i = 0; i < 78; i++) {
             System.out.println("data : " + (i + 1) + "/78");
             JSONObject jsonData = (JSONObject) (apiConnecter.getJSONArray("today-cases-by-provinces")
